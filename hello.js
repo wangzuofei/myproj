a59988932488aff81b21ddeff5e0f417f5343318
wangzuofei <zzunlp@qq.com>
2017-03-28 22:20:15 +0800

    Create hello.js

---
+++ hello.js
@@ -0,0 +1,11 @@
+'use strict';
+var m = new Map();
+var s = new Set();
+alert('你的浏览器支持Map和Set！');
+var arr = ['A', 'B', 'C'];
+var r = arr.filter(function (element, index, self) {
+    console.log(element); // 依次打印'A', 'B', 'C'
+    console.log(index); // 依次打印0, 1, 2
+    console.log(self); // self就是变量arr
+    return true;
+});
